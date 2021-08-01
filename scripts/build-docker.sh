9af70766fb4e1504d38a283ea46f0ba34a996879
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-07-31 21:47:50 -0400

    fix: add

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -3,4 +3,4 @@
 ls -la
 pwd
 docker build -t pulumi:latest /workspace/builder/
-gcloud builds submit --tag gcr.io/intrepid-memory-321513/pulumi:latest
+docker push --tag gcr.io/intrepid-memory-321513/pulumi:latest
