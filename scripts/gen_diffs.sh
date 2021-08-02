e1afd06d8ced5d6a2bdfd689ba3b7b8e5a457a91
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-08-02 14:16:26 -0400

    fix: add

--- scripts/gen_diffs.sh
+++ scripts/gen_diffs.sh
@@ -9,4 +9,12 @@ commit=$(cat commit.txt)
 
 echo "Commit:   $commit"
 echo "Base:     ${BASE_BRANCH}"
+if [[ ! -z "$PR_NUMBER" ]]; then
+  echo "Pull Request: #${PR_NUMBER}"
+fi
+
+git config user.email "<>"
+git config user.name "git"
+git remote add origin ${BASE_REPO_PULL}
+git remove -vv
 
