d508abf76b0f6ebfac0b34d2c6337208e8142377
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-08-02 14:13:16 -0400

    fix: add

--- scripts/gen_diffs.sh
+++ scripts/gen_diffs.sh
@@ -5,4 +5,8 @@ set -e
 git rev-parse HEAD >> commit.txt
 git rev-parse --short HEAD >> short_commit.txt
 
-cat commit.txt
+commit=$(cat commit.txt)
+
+echo "Commit:   $commit"
+echo "Base:     ${BASE_BRANCH}"
+
