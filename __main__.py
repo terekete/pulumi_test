ae2cc518b181e7bd3ee4477b0a2492266d33b366
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-08-03 17:10:36 -0400

    fix: add

--- __main__.py
+++ __main__.py
@@ -5,7 +5,7 @@ from pulumi_gcp import storage, bigquery
 
 # Create a GCP resource (Storage Bucket)
 bucket = storage.Bucket('my-bucket')
-dataset = bigquery.Dataset(resource_name='my-dataset', dataset_id='my_dataset')
+# dataset = bigquery.Dataset(resource_name='my-dataset', dataset_id='my_dataset')
 
 # Export the DNS name of the bucket
 pulumi.export('bucket_name', bucket.url)
