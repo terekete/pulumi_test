98f6e66e2ba72055baf4b6295830217e978b8939
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-08-02 23:41:47 -0400

    fix: add

--- scripts/gen_diffs.sh
+++ scripts/gen_diffs.sh
@@ -39,8 +39,8 @@ fi
 
 ls -la /root/.ssh/config
 git fetch origin "${BASE_BRANCH}":base-branch
-# git checkout base-branch
-# git diff --name-only feature...base-branch > POTENTIAL_CONFLICTS.txt
+git checkout base-branch
+git diff --name-only feature...base-branch > POTENTIAL_CONFLICTS.txt
 
-# echo "Potential conflicts: "
-# cat POTENTIAL_CONFLICTS.txt
+echo "Potential conflicts: "
+cat POTENTIAL_CONFLICTS.txt
