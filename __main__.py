4ad1771c79d3e039df78b24f3477d214a7e3d409
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-08-03 17:21:59 -0400

    fix: add

--- __main__.py
+++ __main__.py
@@ -1,11 +1,17 @@
-"""A Google Cloud Python Pulumi program"""
 
-import pulumi
-from pulumi_gcp import storage, bigquery
+# import pulumi
+# from pulumi_gcp import storage, bigquery
 
-# Create a GCP resource (Storage Bucket)
-bucket = storage.Bucket('my-bucket')
-# dataset = bigquery.Dataset(resource_name='my-dataset', dataset_id='my_dataset')
+# # Create a GCP resource (Storage Bucket)
+# bucket = storage.Bucket('my-bucket')
+# # dataset = bigquery.Dataset(resource_name='my-dataset', dataset_id='my_dataset')
 
-# Export the DNS name of the bucket
-pulumi.export('bucket_name', bucket.url)
+# # Export the DNS name of the bucket
+# pulumi.export('bucket_name', bucket.url)
+
+
+
+import os
+
+with open('DIFF_DATASETS.txt') as reader:
+    print(dir(reader))
