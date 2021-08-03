afb4ebe7fe38a6b68e09efcf5abbb932f3f90f6d
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-08-03 16:21:21 -0400

    fix: add

--- scripts/build_datasets.sh
+++ scripts/build_datasets.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
+export PULUMI_CONFIG_PASSPHRASE=test
+
 pulumi login gs://intrepid-memory-321513-state
 pulumi stack select dev -c --secrets-provider=test
 pulumi config set gcp:project intrepid-memory-321513
 pulumi config set gcp:region northamerica-northeast1
 pulumi version
-export PULUMI_CONFIG_PASSPHRASE=test
 pulumi up --yes
