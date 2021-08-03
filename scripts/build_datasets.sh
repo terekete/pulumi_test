d6f1aebfb4292aaf2f19f90d318419047bfc55f7
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-08-03 12:57:20 -0400

    fix: add

--- scripts/build_datasets.sh
+++ scripts/build_datasets.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
+pulumi login gs://intrepid-memory-321513-state
+pulumi config set gcp:project intrepid-memory-321513
+pulumi config set gcp:region northamerica-northeast1
 pulumi version
-pulumi login
