66196d837b11b8980cf1aa5ed601be5a7d83c6dd
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-08-05 00:12:26 -0400

    fix: add

--- __main__.py
+++ __main__.py
@@ -9,12 +9,20 @@ from pulumi_gcp import storage, bigquery
 # # Export the DNS name of the bucket
 # pulumi.export('bucket_name', bucket.url)
 
+def dataset(manifest):
+    return bigquery.Dataset(
+        resource_name=manifest['resource_name'],
+        dataset_id=manifest['dataset_id'],
+        description=manifest['description'],
+        labels={'cost_center': manifest['cost_center']}
+    )
+
 
 def update(path):
     with open(path + 'manifest.yaml') as f:
         manifest = yaml.safe_load(f)
         if manifest['type'] == 'dataset':
-            bigquery.Dataset(resource_name=manifest['resource_name'], dataset_id=manifest['dataset_id'])
+            dataset(manifest)
         if manifest['type'] == 'table':
             print('create table')
 
@@ -23,3 +31,16 @@ f = open('/workspace/DIFF_LIST.txt')
 for path in f.read().splitlines():
     update(path)
 
+
+# version: v1
+# type: dataset
+# metadata:
+#   cost_center:
+#   dep:
+# team: tsbt
+# description: "Table for FIFA orders"
+# resource_name: tsbt_fifa_orders
+# dataset_id: tsbt_fifa_orders
+# partition_expiration_ms: 1000
+# table_expiration_ms: 1000
+# friendly_name: tsbt_fifa_orders
