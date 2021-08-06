9335c3cfd900c3e5470fe1f4356160e238d8c5a2
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-08-05 22:41:22 -0400

    fix: add

--- __main__.py
+++ __main__.py
@@ -28,9 +28,8 @@ def update(path: str) -> None:
         manifest = yaml.safe_load(f)
         if manifest['type'] == 'dataset':
             dataset(manifest)
-            for reader in manifest['readers']:
-                if reader is not None:
-                    dataset_user_access(manifest, reader, 'READER')
+            for reader in manifest['readers'] or []:
+                dataset_user_access(manifest, reader, 'READER')
         if manifest['type'] == 'table':
             print('create table')
 
