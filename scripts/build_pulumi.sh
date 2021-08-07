540e46962f71d157d33ece37fc449c8f8a6de578
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-08-07 15:01:54 -0400

    fix: add

--- scripts/build_pulumi.sh
+++ scripts/build_pulumi.sh
@@ -7,5 +7,6 @@ pulumi stack select dev -c --secrets-provider=test
 pulumi config set gcp:project intrepid-memory-321513
 pulumi config set gcp:region northamerica-northeast1
 pulumi version
-pulumi refresh
+pulumi refresh --yes
+pulumi preview --yes
 pulumi up --yes
