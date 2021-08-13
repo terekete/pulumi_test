19bb4d7be560b25412f8f5d6cd44dbff41c44d02
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-08-12 21:48:12 -0400

    fix: add

--- __main__.py
+++ __main__.py
@@ -76,11 +76,11 @@ def table_user_access(manifest, table_ref) -> None:
 
 
 def load_manifest(path):
-    with open(path + 'manifest.yaml', 'r') as manifest:
+    manifest = open(path + 'manifest.yaml', 'r')
+        print(dir(manifest))
         try:
             return yaml.safe_load(manifest)
         except yaml.YAMLError as exception:
-            print(dir(manifest))
             raise exception
 
 
