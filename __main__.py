ef652b5f75f1243bea97e574f0035ecfd6fb8bdb
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-08-16 02:14:06 -0400

    fix: add

--- __main__.py
+++ __main__.py
@@ -173,6 +173,7 @@ def pulumi_program():
 stack = pulumi.automation.create_or_select_stack(
     stack_name="dev",
     project_name="intrepid-memory-321513",
-    program=pulumi_program)
+    program=pulumi_program,
+    work_dir="/workspace")
 
 print(stack)
