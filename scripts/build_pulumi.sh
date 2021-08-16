b6a806a34116166ed075636ddc0a8e9e9e9beee9
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-08-16 02:26:28 -0400

    fix: add

--- scripts/build_pulumi.sh
+++ scripts/build_pulumi.sh
@@ -4,12 +4,11 @@ export PULUMI_CONFIG_PASSPHRASE=test
 
 cat /workspace/DIFF_LIST.txt
 
-python3 __main__.py
 
-# pulumi login gs://intrepid-memory-321513-state
-# pulumi stack select dev -c --secrets-provider=test
-# pulumi config set gcp:project intrepid-memory-321513
-# pulumi config set gcp:region northamerica-northeast1
-# pulumi version
-# pulumi refresh --yes
-# pulumi up --yes
+pulumi login gs://intrepid-memory-321513-state
+pulumi stack select dev -c --secrets-provider=test
+pulumi config set gcp:project intrepid-memory-321513
+pulumi config set gcp:region northamerica-northeast1
+pulumi version
+pulumi refresh --yes
+pulumi up --yes
