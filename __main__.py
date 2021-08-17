c7a96a60913f29df8ecf7f5157d7ec7fd436c92b
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-08-16 22:01:16 -0400

    fix: add

--- __main__.py
+++ __main__.py
@@ -1,17 +1,32 @@
 import pulumi
-from pulumi_gcp import storage
+from pulumi_gcp import storage, serviceaccount
+
+
+
+def create_sa(name):
+    sa = serviceaccount.Account(
+        name,
+        account_id=name + "-service-account",
+        display_name=name + "-service-account")
+    # serviceaccount.IAMMember(
+    #     resource_name=name + "-data-editor-iam",
+    #     service_account_id=sa.name,
+    #     role="roles/iam.serviceAccountUser",
+    #     member=sa.email.apply(lambda email: f"serviceAccount:{email}"))
 
 
 def pulumi_program():
-    print("WORKED")
+    print('CURRENT WORKING: ' + os.getcwd())
+    team_path = '/workspace/teams/'
+    team_list = [f for f in os.listdir(team_path) if os.path.isdir(os.path.join(team_path, f))]
+    for team in team_list:
+        create_sa(team)
 
 
 stack = pulumi.automation.create_or_select_stack(
     stack_name="team1",
     project_name="intrepid-memory-321513",
     program=pulumi_program)
-
-print(stack.info())
 
 
 # ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', '_mode', '_run_pulumi_cmd_sync', 'cancel', 'create', 'create_or_select', 'destroy', 'export_stack', 'get_all_config', 'get_config', 'history', 'import_stack', 'info', 'name', 'outputs', 'preview', 'refresh', 'refresh_config', 'remove_all_config', 'remove_config', 'select', 'set_all_config', 'set_config', 'up', 'workspace']
@@ -190,13 +205,6 @@ print(stack.info())
 #         update(path)
 
 
-# # stack = pulumi.automation.create_or_select_stack(
-# #     stack_name="dev",
-# #     project_name="intrepid-memory-321513",
-# #     program=pulumi_program,
-# #     work_dir="/workspace")
-
-# # print(stack)
 
 
 # print('CURRENT WORKING: ' + os.getcwd())
