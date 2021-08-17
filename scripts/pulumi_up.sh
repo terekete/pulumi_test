46b1b05dffbd26955f8ae73b127f489bcf042e33
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-08-16 21:48:24 -0400

    fix: add

--- scripts/pulumi_up.sh
+++ scripts/pulumi_up.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
 pulumi login gs://intrepid-memory-321513-state
+
+export PULUMI_CONFIG_PASSPHRASE=test
+
 # pulumi stack select dev -c --secrets-provider=test
 # pulumi config set gcp:project intrepid-memory-321513
 # pulumi config set gcp:region northamerica-northeast1
