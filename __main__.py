162306fc296610c052f3207cd9989849b59527a0
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-08-17 22:35:20 -0400

    fix: add

--- __main__.py
+++ __main__.py
@@ -3,32 +3,38 @@ import pulumi_gcp as gcp
 import os
 import pulumi.automation as auto
 
-team_path = os.getcwd() + '/teams'
-team_list = [f for f in os.listdir(team_path) if os.path.isdir(os.path.join(team_path, f))]
+
+stack = pulumi.automation.create_or_select_stack(
+    stack_name="dev",
+    project_name="intrepid-memory-321513",
+    program=pulumi_program())
+
+# team_path = os.getcwd() + '/teams'
+# team_list = [f for f in os.listdir(team_path) if os.path.isdir(os.path.join(team_path, f))]
     
-def create_sa(name: str):
-    return gcp.serviceaccount.Account(
-        name,
-        account_id=name + "-service-account",
-        display_name=name + "-service-account")
+# def create_sa(name: str):
+#     return gcp.serviceaccount.Account(
+#         name,
+#         account_id=name + "-service-account",
+#         display_name=name + "-service-account")
 
 
-def create_layer(team: str):
-    return create_sa(team)
+# def create_layer(team: str):
+#     return create_sa(team)
 
 
 
-def pulumi_program():
-    return create_layer("team1")
+# def pulumi_program():
+#     return create_layer("team1")
 
 
-try:
-    stack = pulumi.automation.create_or_select_stack(
-        stack_name="dev",
-        project_name="intrepid-memory-321513",
-        program=pulumi_program())
-    stack.set_config("gcp:region", auto.ConfigValue("northamerica-northeast1"))
-    stack.set_config("gcp:project", auto.ConfigValue("intrepid-memory-321513"))
-    stack.up(on_output=print)
-except auto.StackAlreadyExistsError:
-    print(f"Error: ***")
+# try:
+#     stack = pulumi.automation.create_or_select_stack(
+#         stack_name="dev",
+#         project_name="intrepid-memory-321513",
+#         program=pulumi_program())
+#     stack.set_config("gcp:region", auto.ConfigValue("northamerica-northeast1"))
+#     stack.set_config("gcp:project", auto.ConfigValue("intrepid-memory-321513"))
+#     stack.up(on_output=print)
+# except auto.StackAlreadyExistsError:
+#     print(f"Error: ***")
